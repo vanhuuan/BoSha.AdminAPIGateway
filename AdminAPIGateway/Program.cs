bf331664025122dd1dece68ebe6a86b6c9ce13ad
vanhuuan89 <andeprtai89@gmail.com>
2023-05-16 10:32:53 +0700

    init

---
+++ AdminAPIGateway/Program.cs
@@ -0,0 +1,51 @@
+using Microsoft.IdentityModel.Tokens;
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+using System.Text;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Configuration.SetBasePath(builder.Environment.ContentRootPath)
+    .AddJsonFile("ocelot.json", optional: false, reloadOnChange: true)
+    .AddEnvironmentVariables();
+builder.Configuration.AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+    .AddEnvironmentVariables();
+builder.Services.AddOcelot(builder.Configuration);
+builder.Services.AddSwaggerForOcelot(builder.Configuration);
+builder.Services.AddControllers();
+string[] urls = new string[2];
+urls[0] = "http://localhost:3000";
+urls[1] = "https://bosha-4df95.web.app";
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll",
+        builder =>
+        {
+            builder.WithOrigins(urls)
+            .AllowAnyHeader()
+            .AllowAnyMethod()
+            .AllowCredentials();
+        }
+    );
+});
+string key = builder.Configuration.GetSection("JwtConfig:Secret").Value;
+
+builder.Services.AddAuthentication().AddJwtBearer("Bearer", options =>
+{
+    options.TokenValidationParameters = new TokenValidationParameters()
+    {
+        ValidateIssuerSigningKey = true,
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(key)),
+        ValidateIssuer = false,
+        ValidateAudience = false,
+        ValidateLifetime = true
+    };
+});
+var app = builder.Build();
+app.UseCors("AllowAll");
+app.UseSwaggerForOcelotUI(opt =>
+{
+    opt.PathToSwaggerGenerator = "/swagger/docs";
+});
+app.UseWebSockets();
+await app.UseOcelot();
+app.Run();
---
+++ AdminAPIGateway/Settings/AppSettings.cs
@@ -0,0 +1,33 @@
+﻿using Newtonsoft.Json;
+
+namespace AdminAPIGateway.Settings;
+
+public class AppSettings
+{
+    public Logging Logging { get; set; }
+    public string AllowedHosts { get; set; }
+    public JwtConfig JwtConfig { get; set; }
+}
+
+public class JwtConfig
+{
+    public string Secret { get; set; }
+    public string Secret2 { get; set; }
+    public int ExpirationInHours { get; set; }
+    public int ExpirationInMonths { get; set; }
+}
+
+public class Logging
+{
+    public LogLevel LogLevel { get; set; }
+}
+
+public class LogLevel
+{
+    public string Default { get; set; }
+
+    [JsonProperty("Microsoft.AspNetCore")]
+    public string MicrosoftAspNetCore { get; set; }
+}
+
+
